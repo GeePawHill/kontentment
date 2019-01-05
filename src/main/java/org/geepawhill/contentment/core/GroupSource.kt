0ea0e638b4ea5020405015d695f8e4fd09ace83c
GeePawHill <geepawhill@geepawhill.org>
2019-01-04 22:07:28 -0500

    GroupSource with anonymous rather than lambda

--- src/main/java/org/geepawhill/contentment/core/GroupSource.kt
+++ src/main/java/org/geepawhill/contentment/core/GroupSource.kt
@@ -8,7 +8,6 @@ interface GroupSource {
     class NoGroupSource : RuntimeException("GroupSource has no group.")
 
     companion object {
-
         val NONE: GroupSource = object : GroupSource {
             override fun group(): Group {
                 throw NoGroupSource()
