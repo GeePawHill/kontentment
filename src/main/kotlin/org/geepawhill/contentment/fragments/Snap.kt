b37f806f78601261a6d05aa397e4d293eb12905a
GeePawHill <geepawhill@geepawhill.org>
2019-05-05 14:32:43 -0400

    Added Snap.

---
+++ src/main/kotlin/org/geepawhill/contentment/fragments/Snap.kt
@@ -0,0 +1,37 @@
+package org.geepawhill.contentment.fragments
+
+import javafx.scene.Group
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import org.geepawhill.contentment.core.Context
+import org.geepawhill.contentment.core.Fragment
+import org.geepawhill.contentment.format.Format
+
+/**
+ * Anything loadable into a JavaFx Image.
+ *
+ * @author GeePaw
+ */
+class Snap(owner: Group, private val source: String, width: Double) : Fragment {
+    private val image = Image("/idunno.jpg")
+    private val imageView = ImageView()
+    private var format: Format? = null
+
+    init {
+        owner.children.add(imageView)
+        this.format = Format.DEFAULT
+    }
+
+    override fun prepare(context: Context) {
+        interpolate(context, 0.0)
+    }
+
+    override fun interpolate(context: Context, fraction: Double): Boolean {
+        imageView.image = image
+        return false
+    }
+
+    fun format(format: Format) {
+        this.format = format
+    }
+}
